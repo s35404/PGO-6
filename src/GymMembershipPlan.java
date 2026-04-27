2b2f68cad8d7265fdfcb7129755582d5a6d9a603
s35404 <s35404@pjwstk.edu.pl>
2026-04-27 20:53:33 +0200

    Stworzyłem klasę membership plan.

--- src/GymMembershipPlan.java
+++ src/GymMembershipPlan.java
@@ -1,2 +1,33 @@
 public class GymMembershipPlan extends MembershipPlan {
+private int  entriesPerMonth;
+private boolean saunaAccess;
+
+public GymMembershipPlan(String planCode, String clientName, int months, double baseMonthlyFee,boolean autoRenew, int  entriesPerMonth,boolean saunaAccess){
+    super(planCode, clientName, months, baseMonthlyFee, autoRenew);
+    this.entriesPerMonth = entriesPerMonth;
+    this.saunaAccess = saunaAccess;
+}
+@Override
+public double calculateMonthlyNetPrice() {
+    double monthlyNetPrice = getBaseMonthlyFee() + (entriesPerMonth*4);
+    if(saunaAccess == true){
+        monthlyNetPrice += 25;
+    }
+    if(getAutoRenew() == true){
+        monthlyNetPrice -= 10;
+    }
+    return monthlyNetPrice;
+}
+@Override
+public String getPlanType() {
+    return "Gym Membership Plan";
+}
+@Override
+    public String toString() {
+    return "Gym membersip plan{ " + "Plan Code= " + planCode +
+            '\'' + ", Client name= " + clientName +
+            '\'' + ", Months= " + months +
+            '\'' + ", Base Monthly Fee= " + baseMonthlyFee +
+            '\'' + ", Auto Renew= " + autoRenew;
+}
 }
--- src/MembershipPlan.java
+++ src/MembershipPlan.java
@@ -1,10 +1,9 @@
-public class MembershipPlan {
+abstract class MembershipPlan {
     private String planCode;
     private String clientName;
     private int months;
     private double baseMonthlyFee;
     private boolean autoRenew;
-    private String planType;
 
 
     public MembershipPlan(String planCode, String clientName, int onths, double baseMonthlyFee, boolean autoRenew) {
@@ -14,26 +13,57 @@ public class MembershipPlan {
         this.baseMonthlyFee = baseMonthlyFee;
         this.autoRenew = autoRenew;
     }
+
     public String getPlanCode() {
         return planCode;
     }
+
     public String getClientName() {
         return clientName;
     }
-    public  int getMonths() {
+
+    public int getMonths() {
         return months;
     }
+
     public double getBaseMonthlyFee() {
         return baseMonthlyFee;
     }
-    public boolean isAutoRenew() {
+
+    public boolean getAutoRenew() {
         return autoRenew;
     }
-    public String getPlanType() {
-        return planType;
-    }
-    public double calculateMonthlyNetPrice() {
-        double monthlyNetPrice = 0;
+
+    public abstract String getPlanType();
+
+    public abstract double calculateMonthlyNetPrice();
+
+    public double calculateMonthlyGrossPrice(){
+        return baseMonthlyFee ;
     }
 
+    double calculateTotalNetPrice() {
+        return calculateMonthlyNetPrice()*months;
+    }
+    public final void printSummary() {
+        System.out.println("Plan Code: " + planCode);
+        System.out.println("Client Name: " + clientName);
+        System.out.println("Months: " + months);
+        System.out.println("Base Monthly Fee: " + baseMonthlyFee);
+        System.out.println("Auto Renew: " + autoRenew);
+        System.out.println("Monthly Net Price: " + calculateMonthlyNetPrice());
+        System.out.println("Monthly Gross Price: " + calculateMonthlyGrossPrice());
+        System.out.println("Total Net Price: " + calculateTotalNetPrice());
+
+    }
+    @Override
+    public String toString() {
+        return "Membership Plan{" + "Plan Code= " + planCode +
+                '\'' + ", Client name= " + clientName +
+                '\'' + ", Months= " + months +
+                '\'' + ", Base Monthly Fee= " + baseMonthlyFee +
+                '\'' + ", Auto Renew= " + autoRenew;
+
+
+    }
  }
